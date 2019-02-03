f656e997990d2304294d53f6576eea39b814f05e
guozishan <47099331+guozishan@users.noreply.github.com>
2019-02-03 16:16:21 +0800

    test

--- resources/views/postmessage.blade.php
+++ resources/views/postmessage.blade.php
@@ -1,3 +1,4 @@
+//展示留言的界面
 <?php foreach ($msgRes as $msg):?>
 <label>
     <span title="<?php echo $msg->email;?>"><?php echo $msg->uname;?> :</span>
@@ -5,3 +6,6 @@
     <font><?php echo date("Y-m-d H:i:s",$msg->addtime);?><a href="###">删除</a> <a href="###">修改</a></font><br/>
 </label>
 <?php endforeach;?>
+
+//删除留言的链接
+<a href="<?php echo URL::action('DeleteController@del',['id'=>$post->id]); ?>">删除</a>
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Route::get('index', 'AddPostController@index');//留言列表路由
 Route::get('add', 'AddPostController@add');//留言添加界面展示
 Route::post('add', 'AddPostController@addPost');//处理留言添加
 Route::match(['get','post'],'edit/{id}', 'AddPostController@edit')->where('id','[0-9]{1,3}');//同时处理留言编辑的界面展示和提交修改留言功能
-Route::get('del/{id}', 'AddPostController@del');//删除留言
+Route::get('del/{id}', '
+    DeleteController@del')->
+    where('id','[0-9]{1,3}') ;//删除留言
