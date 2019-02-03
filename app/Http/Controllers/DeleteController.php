ccc0d262dec2ace3aa9e1fccdfa37c7631b7c383
guozishan <47099331+guozishan@users.noreply.github.com>
2019-02-03 16:48:15 +0800

    test

---
+++ app/Http/Controllers/DeleteController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 田家硕
+ * Date: 2018/12/12
+ * Time: 18:55
+ */
+
+namespace App\Http\Controllers;
+
+
+class DeleteController extends Controller
+{
+   public function changeBlog($id){
+   	$uid = $id;
+   	$affected = DB::update('update msg set flag = 0 where id =?',[$uid]) 
+   }
+
+   public function displayBlog(){
+   	$Blogs = DB::select('select content from msg where flag = ?',[1]);
+   	return view('postmessage',['content' => $Blogs]); 
+
+   }
+
+}
